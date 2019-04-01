732f488e938e0e3d5e8b008507d3c1af06ca1041
silvercon <silverconx@outlook.com>
2019-04-01 14:03:49 +0800

    国家接口整理

--- src/test/java/com/cdkj/gchf/gov/Corp.java
+++ src/test/java/com/cdkj/gchf/gov/Corp.java
@@ -33,4 +33,37 @@ public class Corp {
         // corp-upload-2019031419-1-1370
     }
 
+    @Test
+    public void corpBadRecordQuery() {
+        Map<String, String> dataMap = new HashMap<String, String>();
+        dataMap.put("pageIndex", "0");
+        dataMap.put("pageSize", "10");
+        dataMap.put("corpCode", "913311001484116584");
+        dataMap.put("corpName", "正达建设有限公司");
+
+        String data = JSONObject.toJSON(dataMap).toString();
+
+        System.out.println(data);
+
+        GovConnecter.getGovData("CorpBadRecord.Query", data,
+            "33112220190310002", "24484b262dd63dd584902a266bdbdca0");
+
+    }
+
+    @Test
+    public void corpGoodRecordQuery() {
+        Map<String, String> dataMap = new HashMap<String, String>();
+        dataMap.put("pageIndex", "0");
+        dataMap.put("pageSize", "10");
+        dataMap.put("corpCode", "913311001484116584");
+        dataMap.put("corpName", "正达建设有限公司");
+
+        String data = JSONObject.toJSON(dataMap).toString();
+
+        System.out.println(data);
+
+        GovConnecter.getGovData("CorpGoodRecord.Query", data,
+            "33112220190310002", "24484b262dd63dd584902a266bdbdca0");
+
+    }
 }

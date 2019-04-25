7d19f7ff432c01365295717718efee2740a3c8dc
silvercon <silverconx@outlook.com>
2019-04-25 16:36:06 +0800

    上传状态更新修改

--- src/main/java/com/cdkj/gchf/common/QiniuUtil.java
+++ src/main/java/com/cdkj/gchf/common/QiniuUtil.java
@@ -1,9 +1,15 @@
 package com.cdkj.gchf.common;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class QiniuUtil {
 
     public static String parseUrl(String token) {
         String qiniuUrl = null;
+
+        if (StringUtils.isBlank(token)) {
+            return null;
+        }
 
         if (token.startsWith("data:image")) {
             return token;
